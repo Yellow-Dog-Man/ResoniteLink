1856dc60e55c478ede014fe6e0018894e35641c9
Frooxius <tomas.mariancik@gmail.com>
2026-01-02 14:46:29 -0800

    feat: Add list support

---
+++ Models/DataModel/SyncList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace ResoniteLink
+{
+    public class SyncList : Member
+    {
+        [JsonPropertyName("elements")]
+        public List<Member> Elements { get; set; }
+    }
+
+    [JsonDerivedType(typeof(SyncList), "list")]
+    public partial class Member { }
+}
