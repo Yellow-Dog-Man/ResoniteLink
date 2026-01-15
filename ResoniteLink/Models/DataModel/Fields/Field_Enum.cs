fd98e130ab3daf621f426d2857932d414377d2bc
Frooxius <tomas.mariancik@gmail.com>
2026-01-15 22:39:52 +0100

    feat: Add nullable enum

--- ResoniteLink/Models/DataModel/Fields/Field_Enum.cs
+++ ResoniteLink/Models/DataModel/Fields/Field_Enum.cs
@@ -21,6 +21,23 @@ namespace ResoniteLink
         public override Type ValueType => typeof(Enum);
     }
 
+    public class Field_Nullable_Enum : Field
+    {
+        [JsonPropertyName("value")]
+        public string Value { get; set; }
+
+        [JsonPropertyName("enumType")]
+        public string EnumType { get; set; }
+
+        [JsonIgnore]
+        public override object BoxedValue { get => Value; set => Value = value as string; }
+
+        // We don't actually have the enum type, so we just give general enum type
+        [JsonIgnore]
+        public override Type ValueType => typeof(Enum);
+    }
+
     [JsonDerivedType(typeof(Field_Enum), "enum")]
+    [JsonDerivedType(typeof(Field_Nullable_Enum), "enum?")]
     public partial class Member { }
 }
