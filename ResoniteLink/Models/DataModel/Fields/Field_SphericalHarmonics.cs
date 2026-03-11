8c87f526b62daaae66a2082a87170fbce0f6f9f2
Frooxius <tomas.mariancik@gmail.com>
2026-03-11 18:16:34 +0100

    feat: Added support for spherical harmonics types

---
+++ ResoniteLink/Models/DataModel/Fields/Field_SphericalHarmonics.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace ResoniteLink
+{
+    public class Field_SH_L1 : Field
+    {
+        [JsonPropertyName("value")]
+        public SphericalHarmonicsL1 Value { get; set; }
+
+        [JsonIgnore]
+        public override object BoxedValue { get => Value; set => Value = (SphericalHarmonicsL1)value; }
+        [JsonIgnore]
+        public override Type ValueType => typeof(SphericalHarmonicsL1);
+    }
+
+    public class Field_SH_L2 : Field
+    {
+        [JsonPropertyName("value")]
+        public SphericalHarmonicsL2 Value { get; set; }
+
+        [JsonIgnore]
+        public override object BoxedValue { get => Value; set => Value = (SphericalHarmonicsL2)value; }
+        [JsonIgnore]
+        public override Type ValueType => typeof(SphericalHarmonicsL2);
+    }
+
+    public class Field_SH_L3 : Field
+    {
+        [JsonPropertyName("value")]
+        public SphericalHarmonicsL3 Value { get; set; }
+
+        [JsonIgnore]
+        public override object BoxedValue { get => Value; set => Value = (SphericalHarmonicsL3)value; }
+        [JsonIgnore]
+        public override Type ValueType => typeof(SphericalHarmonicsL3);
+    }
+
+    public class Field_SH_L4 : Field
+    {
+        [JsonPropertyName("value")]
+        public SphericalHarmonicsL4 Value { get; set; }
+
+        [JsonIgnore]
+        public override object BoxedValue { get => Value; set => Value = (SphericalHarmonicsL4)value; }
+        [JsonIgnore]
+        public override Type ValueType => typeof(SphericalHarmonicsL4);
+    }
+
+    [JsonDerivedType(typeof(Field_SH_L1), "sh_l1")]
+    [JsonDerivedType(typeof(Field_SH_L2), "sh_l2")]
+    [JsonDerivedType(typeof(Field_SH_L3), "sh_l3")]
+    [JsonDerivedType(typeof(Field_SH_L4), "sh_l4")]
+    public partial class Member { }
+}
